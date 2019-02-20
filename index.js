a299a63a5273216d42eaffe21d85f3ecf1673769
Joe Tustin <j.tustin@gmail.com>
2019-02-20 23:50:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 // `findBy(array, findFn)` which looks for a value in `array` based on the return value of `findFn`. Return `null` if the
 //  value isn't found. **Hint:** *Check the test file to know the signature of the findFn and the type of its return value*
-
+// by setting the findFn to the passed function it was able to plug in the value and test True/False
 /*
 function findBy(array, findFn) {
   for(var i = 0; i < array.length; i++) {
