2e8ebb2776e07dd04d15cce928467c34be822bf3
Joe Tustin <j.tustin@gmail.com>
2019-02-20 21:02:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,10 @@
 //          value isn't found. **Hint:** *Check the test file to know the signature of the findFn and the type of its return value*
 
 
+// I thought I would use .forEach but per google it's the wrong tool to use
 
 function breakOut(array, changeValue, stopValue) {
-  
+  for(var i = 0; i > array.length; ++i) {
+    
+  }
 }
