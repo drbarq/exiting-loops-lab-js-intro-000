09c9fd1afc45045ae4b3c2040b23907efcfec5a4
Joe Tustin <j.tustin@gmail.com>
2019-02-20 21:51:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 
 // - `breakOut(array, changeValue, stopValue)` which iterates through `array` and changes every element to `changeValue`
 //          until the loop reaches `stopValue`. Then we `break` out of the loop and return the array.
-// `keepGoing(array, changeValue, skipValue)` which iterates through `array` and changes every element to `changeValue`
-//          except those that match `skipValue`. Then return the array.
+
 // `findBy(array, findFn)` which looks for a value in `array` based on the return value of `findFn`. Return `null` if the
 //          value isn't found. **Hint:** *Check the test file to know the signature of the findFn and the type of its return value*
 
@@ -27,3 +26,31 @@ function breakOut(array, changeValue, stopValue) {
   return array
 // console.log(array)
 }
+
+
+// `keepGoing(array, changeValue, skipValue)` which iterates through `array` and changes every element to `changeValue`
+//          except those that match `skipValue`. Then return the array.
+// im thinking i can either set the variable equal to itself and in essence skips it, or leave it alone with continue and go back to the top
+
+function keepGoing(array, changeValue, skipValue) {
+  for(var i = 0; i < array.length; ++i) {
+    if (array[i] != skipValue) {
+    //  array[i] = changeValue;
+      array.splice(i, 1, changeValue)
+     }
+     else {
+        continue
+     }
+  }
+  return array
+// console.log(array)
+}
+}
+
+
+
+
+
+
+
+
