da91475dc0bc257949b78e47d7015bd55ed4876e
Joe Tustin <j.tustin@gmail.com>
2019-02-20 22:03:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,6 @@
 // - `breakOut(array, changeValue, stopValue)` which iterates through `array` and changes every element to `changeValue`
 //          until the loop reaches `stopValue`. Then we `break` out of the loop and return the array.
 
-// `findBy(array, findFn)` which looks for a value in `array` based on the return value of `findFn`. Return `null` if the
-//          value isn't found. **Hint:** *Check the test file to know the signature of the findFn and the type of its return value*
-
-
 // I thought I would use .forEach but per google it's the wrong tool to use
 //I used a for loop and an if statement to check if the element was the same as the stop value
 // if it wasnt (true?) then change the value with the changevalue variable
@@ -31,7 +27,7 @@ function breakOut(array, changeValue, stopValue) {
 // `keepGoing(array, changeValue, skipValue)` which iterates through `array` and changes every element to `changeValue`
 //          except those that match `skipValue`. Then return the array.
 // im thinking i can either set the variable equal to itself and in essence skips it, or leave it alone with continue and go back to the top
-// to change the value, I can either delete and reset or set the value:: is there a difference?  
+// to change the value, I can either delete and reset or set the value:: is there a difference?
 function keepGoing(array, changeValue, skipValue) {
   for(var i = 0; i < array.length; ++i) {
     if (array[i] != skipValue) {
@@ -44,4 +40,15 @@ function keepGoing(array, changeValue, skipValue) {
   }
   return array
 // console.log(array)
+}
+
+
+// `findBy(array, findFn)` which looks for a value in `array` based on the return value of `findFn`. Return `null` if the
+//          value isn't found. **Hint:** *Check the test file to know the signature of the findFn and the type of its return value*
+
+
+
+function findBy(array, findFn) {
+
+
 }
