3f1d702f34db3cc172327ff3b66adce6712cb848
Joe Tustin <j.tustin@gmail.com>
2019-02-20 22:11:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,8 +49,10 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for(var i = 0; i < array.length; i++) {
     if (array[i] === findFn) {
-      console.log(array[i])
-      return array[i]
+      console.log(findFn)
+      return findFn  
+    //  console.log(array[i])
+    //  return array[i]
      }
     else {
       return null
