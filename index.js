f2f0db460d88385e3b44c29d9cdc73dff6131d58
Joe Tustin <j.tustin@gmail.com>
2019-02-20 21:31:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,7 @@
 function breakOut(array, changeValue, stopValue) {
   for(var i = 0; i < array.length; ++i) {
     if (array[i] != stopValue) {
-      array.splice(array[i], 1, changeValue);
-      return array[i]
+      array.splice(i, 1, changeValue);
      }
   }
 }
