fc44e18aa05520a843fd92eaf3fd942ff4aef952
Joe Tustin <j.tustin@gmail.com>
2019-02-20 23:51:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,30 +46,7 @@ function keepGoing(array, changeValue, skipValue) {
 // `findBy(array, findFn)` which looks for a value in `array` based on the return value of `findFn`. Return `null` if the
 //  value isn't found. **Hint:** *Check the test file to know the signature of the findFn and the type of its return value*
 // by setting the findFn to the passed function it was able to plug in the value and test True/False
-/*
-function findBy(array, findFn) {
-  for(var i = 0; i < array.length; i++) {
-      if (array.find(function(findFn) {
-        console.log(array[i])
-      });
-      else {
-        console.log(null)
-        return null
-      }
-    }
-}
-*/
-/*
-function findBy(array, findFn) {
-  for (let i = 0; i < array.length; i++) {
-    if ((array[i] => array[i] === findFn)(array[i])) {
-      console.log(findFn)
-      return array[i]
-    }
-  }
-  return null
-}
-*/
+
 
 function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
@@ -80,26 +57,3 @@ function findBy(array, findFn) {
   }
   return null
 }
-
-/*
-
-if (criteriaFunc(array[i])) {
-     return array[i]
-/*
-
-    //    array[i] = findFn) {
-      //  console.log(findFn)
-        console.log(array[i] === findFn)
-  //    return array[i]
-})); else {
-
-  console.log(null)
-  return null
-
-  }
-  }
-
-}
-
-//
-*/
